7dfd799f685e538636c8260f8ebc95a158d6aea8
evaseemefly <evaseemefly@126.com>
2021-07-18 22:04:45 +0800

    title:21-07-18 加入了 wms 的方式加载 grid_shape 发布的网格

--- webclient/src/router/router.js
+++ webclient/src/router/router.js
@@ -11,63 +11,96 @@ import fubcontent from '../layout/content/center_map_fub.vue'
 // 引入风暴潮content
 import stormcontent from '../layout/content/center_map_storm.vue'
 // 路由改为组件的方式引入，不再使用require
-const routers = [
-  {
-    path: '/home',
-    name: 'home',
-    component: home,
-    children: [
-      // {
-      //   name: 'content',
-      //           // path:'content/:did',
-      //   // path: 'content/:code/:category',
-      //   path: 'content',
-      //   // component: content,
-      //   children: [
-      //     {
-      //       name: 'storm',
-      //       path: 'storm/:code',
-      //       component: content
-      //     },
-      //     {
-      //       name: 'grid',
-      //       path: 'grid/:code',
-      //       component: content
-      //     },
-      //     {
-      //       name: 'forecast',
-      //       path: 'forecast/:code',
-      //       component: contentForecast
-      //     }
+// const routers = [
+//   {
+//     path: '/home',
+//     name: 'home',
+//     component: home,
+//     children: [
+//       // {
+//       //   name: 'content',
+//       //           // path:'content/:did',
+//       //   // path: 'content/:code/:category',
+//       //   path: 'content',
+//       //   // component: content,
+//       //   children: [
+//       //     {
+//       //       name: 'storm',
+//       //       path: 'storm/:code',
+//       //       component: content
+//       //     },
+//       //     {
+//       //       name: 'grid',
+//       //       path: 'grid/:code',
+//       //       component: content
+//       //     },
+//       //     {
+//       //       name: 'forecast',
+//       //       path: 'forecast/:code',
+//       //       component: contentForecast
+//       //     }
 
-      //   ]
+//       //   ]
 
-      // },
-      {
-        name: 'storm',
-        path: 'content/storm/:code',
-        component: stormcontent
-      },
-      {
-        name: 'grid',
-        path: 'content/grid/:code',
-        component: gridcontent
-      },
-      {
-        name: 'forecast',
-        path: 'content/forecast/:code',
-        component: contentForecast
-      },
-      {
-        name: 'fub',
-        path: 'content/fub/:code',
-        component: fubcontent
-      }
-    ]
-  }
-  // {
-  //     path:'/login',
-  //     component:login
-  // }
-]
-export default routers
+//       // },
+//       {
+//         name: 'storm',
+//         path: 'content/storm/:code',
+//         component: stormcontent
+//       },
+//       {
+//         name: 'grid',
+//         path: 'content/grid/:code',
+//         component: gridcontent
+//       },
+//       {
+//         name: 'forecast',
+//         path: 'content/forecast/:code',
+//         component: contentForecast
+//       },
+//       {
+//         name: 'fub',
+//         path: 'content/fub/:code',
+//         component: fubcontent
+//       }
+//     ]
+//   }
+// ]
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      // 注意以/ 开头的嵌套路径会被当做根路径
+      path: '/home',
+      component: home,
+      children: [
+        {
+          name: 'storm',
+          path: 'content/storm/:code',
+          component: stormcontent
+        },
+        {
+          name: 'grid',
+          path: 'content/grid/:code',
+          component: gridcontent
+        },
+        {
+          name: 'forecast',
+          path: 'content/forecast/:code',
+          component: contentForecast
+        },
+        {
+          name: 'fub',
+          path: 'content/fub/:code',
+          component: fubcontent
+        }
+      ]
+    }
+  ]
+})
+
+// export default routers
